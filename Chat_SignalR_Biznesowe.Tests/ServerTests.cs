06a27573f8bbafa1d8ff28519f30fb3fc7b6f495
AkenaiVelo <byakuyavelo@gmail.com>
2015-06-25 22:00:34 +0200

    Fixed repo issues.

---
+++ Chat_SignalR_Biznesowe.Tests/ServerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Reflection;
+
+using Chat_SignalR_Biznesowe;
+using Chat_SignalR_Biznesowe.SignalRThings;
+using System.Threading.Tasks;
+
+namespace Chat_SignalR_Biznesowe.Tests
+{
+    [TestClass]
+    public class ServerTests
+    {
+        private Server sut;
+
+        [TestInitialize]
+        public void Startup()
+        {
+            sut = new Server();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.MissingMemberException))]
+        public void Test_Server_Started_With_Bad_IP()
+        {
+            sut.StartServer("bad_ip");
+        }
+
+        [TestMethod]       
+        public void Test_Server_Started_With_Good_IP()
+        {
+            Task.Run(() => sut.StartServer("http://localhost:8080"));
+        }
+
+        [TestCleanup]
+        public void Clear()
+        {
+            sut = null;
+        }
+    }
+}
---
+++ Chat_SignalR_Biznesowe/SignalRThings/Server.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat_SignalR_Biznesowe.SignalRThings
+{
+    public class Server : Chat_SignalR_Biznesowe.SignalRThings.IServer
+    {
+        public IDisposable SignalR { get; set; }
+     //   const string ServerURI = "http://localhost:8080";
+
+        public void StartServer(String serverURI)
+        {
+            try
+            {
+                SignalR = WebApp.Start(serverURI);
+
+                
+            }
+            catch (TargetInvocationException ex)
+            {
+                Debug.WriteLine(ex.Message.ToString());
+                return;
+            }
+        }
+
+        public void DisposeServer()
+        {
+            SignalR.Dispose();
+        }
+
+    }
+}
